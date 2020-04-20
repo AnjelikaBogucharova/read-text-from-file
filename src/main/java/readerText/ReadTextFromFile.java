aa9a210a9ca7e3e18baba33a565871e53c2276b6
AnjelikaBogucharova <lika.bogucharova@mail.ru>
2020-04-20 21:23:37 +0300

    Добавлена новая реализация чтения содержимого из текстового файла.

--- src/main/java/readerText/ReadTextFromFile.java
+++ src/main/java/readerText/ReadTextFromFile.java
@@ -4,10 +4,10 @@ public class ReadTextFromFile {
     public static void main(String[] args) throws Exception {
         ReadTxt readFile = new ReadTxt();
 
-        System.out.println("simpleText");
+        System.out.println("Чтение содиржимого из файла: simpleText.txt");
         System.out.println(readFile.textFromFile("src/main/resources/simpleText.txt","UTF-8"));
 
-        System.out.println("oneMoreText");
+        System.out.println("\r\nЧтение содиржимого из файла: oneMoreText.txt");
         System.out.println(readFile.textFromFile("src/main/resources/oneMoreText.txt","Windows-1251"));
     }
 }
